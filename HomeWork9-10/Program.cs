4e827557f49accb4ae6c3f5876c9cf76921addbd
Marat <pirat03071988@mail.ru>
2022-07-21 20:39:45 +0200

    Я студентНе судите строго. Только изучаю GitHub

---
+++ HomeWork9-10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork9_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет гребанный Гит хаб");
+        }
+    }
+}
