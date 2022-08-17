8bedea1158532ee1a53a281b71c2dbf34271dd74
Marat <pirat03071988@mail.ru>
2022-08-17 21:59:36 +0200

    UPDATE

---
+++ Exam1/MyDataBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace MyDBHelper
+{
+    public static class MyDataBase
+    {
+        const string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=dictionary.mdb";
+        private static OleDbConnection connection;
+        private static OleDbCommand command;
+        private static void OpenConnection(string strSQL)
+        {
+            connection = new OleDbConnection(connectionString);
+            command = new OleDbCommand(strSQL, connection);
+            connection.Open();
+        }
+        //Отображение данных таблицы
+        public static void ShowInfo()
+        {
+            string strSQL = "SELECT * FROM RusEng ORDER BY 2,3";
+            try
+            {
+                OpenConnection(strSQL);
+                using (OleDbDataReader reader = command.ExecuteReader())
+                {
+                    Console.WriteLine("id\t\tСлово\t\t\tПеревод");
+                    Console.WriteLine("======================================================");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0} \t\t{1}", reader["id"], reader["Word"]);
+                        Console.SetCursorPosition(40, Console.CursorTop - 1);
+                        Console.WriteLine(reader["Translate"]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            connection.Close();
+        }
+        //Добавление данных
+        public static void InsertData(string word, string translateWord)
+        {
+            string strSQL = "INSERT INTO RusEng ([Word], [Translate]) VALUES('" + word + "','" + translateWord + "')";
+            try
+            {
+                OpenConnection(strSQL);
+                using (command.ExecuteReader())
+                {
+                    Console.WriteLine("Успешное добавление в базу данных");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            connection.Close();
+        }
+        //Удаление данных по id
+        public static void DeleteData(int id)
+        {
+            string strSQL = "DELETE FROM RusEng WHERE id = " + id;
+            try
+            {
+                OpenConnection(strSQL);
+                using (command.ExecuteReader())
+                {
+                    Console.WriteLine("Успешное удаление из базы данных");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            connection.Close();
+        }
+    }
+}
--- Exam1/Program.cs
+++ Exam1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.OleDb;
+using MyDBHelper;
 
 namespace Exam1
 {
@@ -11,39 +12,7 @@ namespace Exam1
     {
         static void Main(string[] args)
         {
-            //Строка подключения и SQL-запрос
-            string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=dictionary.mdb";
-            string strSQL = "SELECT * FROM RusEng";
-            // Создаем соединение    
-            using (OleDbConnection connection = new OleDbConnection(connectionString))
-            {
-                // Создаем команду и устанавливаем ее подключение    
-                OleDbCommand command = new OleDbCommand(strSQL, connection);
-                // Открываем соединение и выполняем команду select.    
-                try
-                {
-                    // Открыть соединение    
-                    connection.Open();
-                    // Выполнить команду   
-                    using (OleDbDataReader reader = command.ExecuteReader())
-                    {
-                        Console.WriteLine("****************Англо-русский словарь****************");
-                        Console.WriteLine("id\t\tСлово\t\t\tПеревод");
-                        Console.WriteLine("======================================================");
-                        while (reader.Read())
-                        {
-                            Console.WriteLine("{0} \t\t{1}", reader["id"], reader["RusWord"]);
-                            Console.SetCursorPosition(40, Console.CursorTop - 1);
-                            Console.WriteLine(reader["EngWord"]);
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                // Соединение автоматически закрывается из-за using блока   
-            }
+            MyDataBase.ShowInfo();
             Console.ReadKey();
         }
     }
