d9b3d4fd1a339bd603bc52f3da588eef3d47e401
Marat <108996479+Marat1988@users.noreply.github.com>
2022-09-19 11:09:31 +0200

    UPDATE

--- Exam2/StartQuiz.Designer.cs
+++ Exam2/StartQuiz.Designer.cs
@@ -30,27 +30,59 @@ namespace WinFormsApp1
         private void InitializeComponent()
         {
             this.labelQuestion = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelQuestion
             // 
             this.labelQuestion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.labelQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelQuestion.Location = new System.Drawing.Point(12, 9);
+            this.labelQuestion.Location = new System.Drawing.Point(9, 7);
+            this.labelQuestion.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelQuestion.Name = "labelQuestion";
-            this.labelQuestion.Size = new System.Drawing.Size(965, 240);
+            this.labelQuestion.Size = new System.Drawing.Size(766, 271);
             this.labelQuestion.TabIndex = 0;
             this.labelQuestion.Text = "labelQuestion";
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(4, 30);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 289);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(215, 150);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
             // StartQuiz
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(989, 542);
+            this.ClientSize = new System.Drawing.Size(805, 537);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.labelQuestion);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "StartQuiz";
             this.Text = "Викторина";
             this.Load += new System.EventHandler(this.StartQuiz_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -58,5 +90,7 @@ namespace WinFormsApp1
         #endregion
 
         private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
