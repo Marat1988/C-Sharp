a18669603dcebb896b9abcfee5e0c88d6d2d6bd6
Marat <108996479+Marat1988@users.noreply.github.com>
2022-09-19 09:02:55 +0200

    UPDATE

---
+++ Exam2/StartQuiz.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class StartQuiz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelQuestion.Location = new System.Drawing.Point(12, 9);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(965, 240);
+            this.labelQuestion.TabIndex = 0;
+            this.labelQuestion.Text = "labelQuestion";
+            // 
+            // StartQuiz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 542);
+            this.Controls.Add(this.labelQuestion);
+            this.Name = "StartQuiz";
+            this.Text = "Викторина";
+            this.Load += new System.EventHandler(this.StartQuiz_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelQuestion;
+    }
+}
