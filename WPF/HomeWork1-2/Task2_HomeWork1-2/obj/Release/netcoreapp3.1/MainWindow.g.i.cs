3babe6f913e97f563c0220decf37b8e1edba63cb
Marat <pirat03071988@mail.ru>
2022-10-06 23:02:26 +0200

    UPDATE

--- WPF/HomeWork1-2/Task2_HomeWork1-2/obj/Release/netcoreapp3.1/MainWindow.g.i.cs
+++ WPF/HomeWork1-2/Task2_HomeWork1-2/obj/Release/netcoreapp3.1/MainWindow.g.i.cs
@@ -229,7 +229,7 @@ namespace Task2_HomeWork1_2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Task2_HomeWork1-2;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Task2_HomeWork1-2;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
