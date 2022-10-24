7abb9aebd7d5ab474c616af5a518341ad76a77ae
Marat <108996479+Marat1988@users.noreply.github.com>
2022-10-24 14:49:36 +0200

    UPDATE

---
+++ WindowsForm/HomeWork3-4/Task2/FormMain.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace Task2
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.textBoxFolderPath = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.groupBoxDisplayTypeFiles = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBoxDisplayTypeFiles.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 165);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(980, 374);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "files.png");
+            // 
+            // textBoxFolderPath
+            // 
+            this.textBoxFolderPath.Location = new System.Drawing.Point(12, 17);
+            this.textBoxFolderPath.Name = "textBoxFolderPath";
+            this.textBoxFolderPath.ReadOnly = true;
+            this.textBoxFolderPath.Size = new System.Drawing.Size(322, 22);
+            this.textBoxFolderPath.TabIndex = 1;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(374, 12);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(94, 32);
+            this.buttonSearch.TabIndex = 2;
+            this.buttonSearch.Text = "Показать";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // groupBoxDisplayTypeFiles
+            // 
+            this.groupBoxDisplayTypeFiles.Controls.Add(this.radioButton5);
+            this.groupBoxDisplayTypeFiles.Controls.Add(this.radioButton4);
+            this.groupBoxDisplayTypeFiles.Controls.Add(this.radioButton3);
+            this.groupBoxDisplayTypeFiles.Controls.Add(this.radioButton2);
+            this.groupBoxDisplayTypeFiles.Controls.Add(this.radioButton1);
+            this.groupBoxDisplayTypeFiles.Location = new System.Drawing.Point(12, 45);
+            this.groupBoxDisplayTypeFiles.Name = "groupBoxDisplayTypeFiles";
+            this.groupBoxDisplayTypeFiles.Size = new System.Drawing.Size(322, 114);
+            this.groupBoxDisplayTypeFiles.TabIndex = 3;
+            this.groupBoxDisplayTypeFiles.TabStop = false;
+            this.groupBoxDisplayTypeFiles.Text = "Тип отображения:";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(155, 48);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(52, 21);
+            this.radioButton5.TabIndex = 8;
+            this.radioButton5.Tag = "4";
+            this.radioButton5.Text = "Tile";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(155, 21);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(51, 21);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.Tag = "3";
+            this.radioButton4.Text = "List";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 75);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(93, 21);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.Tag = "2";
+            this.radioButton3.Text = "Small icon";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 48);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(72, 21);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.Tag = "1";
+            this.radioButton2.Text = "Details";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(96, 21);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Tag = "0";
+            this.radioButton1.Text = "Large icon";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 569);
+            this.Controls.Add(this.groupBoxDisplayTypeFiles);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxFolderPath);
+            this.Controls.Add(this.listView1);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ListView";
+            this.groupBoxDisplayTypeFiles.ResumeLayout(false);
+            this.groupBoxDisplayTypeFiles.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBoxFolderPath;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.GroupBox groupBoxDisplayTypeFiles;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ WindowsForm/HomeWork3-4/Task2/FormMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            listView1.View = (View)int.Parse(groupBoxDisplayTypeFiles.Controls.OfType<RadioButton>().FirstOrDefault(item => item.Checked == true).Tag.ToString());
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath); //Получаем все файлы
+                    if (files.Length == 0)
+                    {
+                        MessageBox.Show("Файлы не найдены", "Результат поиска", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        textBoxFolderPath.Text = fbd.SelectedPath;
+                        listView1.Items.Clear();
+                        foreach (string file in files)
+                        {
+                            ListViewItem lvi = new ListViewItem();
+                            //установка названия файла
+                            lvi.Text = file.Remove(0, file.LastIndexOf('\\') + 1);
+                            lvi.ImageIndex = 0; //установка картинки для файла
+                            //Добавляем элемент в ListView
+                            listView1.Items.Add(lvi);
+                        }
+                    }
+                }
+            }
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            if (radioButton.Checked)
+            {
+
+                listView1.View = (View)int.Parse(radioButton.Tag.ToString());
+            }
+        }
+    }
+}
