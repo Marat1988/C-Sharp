906172ae5762693b89ce98c3cab69beade2afd14
Marat <pirat03071988@mail.ru>
2022-10-29 18:20:58 +0200

    UPDATE

---
+++ WindowsForm/HomeWork7-8/Task5/FormMain.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Task5
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(363, 184);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(97, 23);
+            this.buttonRun.TabIndex = 0;
+            this.buttonRun.Text = "Кликни на меня";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            this.buttonRun.MouseMove += new System.Windows.Forms.MouseEventHandler(this.buttonRun_MouseMove);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 377);
+            this.Controls.Add(this.buttonRun);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Убегающий статик";
+            this.SizeChanged += new System.EventHandler(this.FormMain_SizeChanged);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRun;
+    }
+}
+
---
+++ WindowsForm/HomeWork7-8/Task5/FormMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task5
+{
+    public partial class FormMain : Form
+    {
+        Random random = new Random();
+        public FormMain()
+        {
+            InitializeComponent();
+            this.MinimumSize = new Size(buttonRun.Width + 40, buttonRun.Height + 70);
+
+        }
+
+        private void randomLeftTopButton()
+        {
+            buttonRun.Left = random.Next(0, ClientSize.Width - buttonRun.Width);
+            buttonRun.Top = random.Next(0, ClientSize.Height - buttonRun.Height);
+        }
+
+        private void buttonRun_MouseMove(object sender, MouseEventArgs e) => randomLeftTopButton();
+
+        private void buttonRun_Click(object sender, EventArgs e) => MessageBox.Show("Ура! Поймал", "Победа!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        private void FormMain_SizeChanged(object sender, EventArgs e)
+        {
+            if (ClientSize.Width <= buttonRun.Left + buttonRun.Width || ClientSize.Height <= buttonRun.Top + buttonRun.Height)
+                randomLeftTopButton();
+        }
+    }
+}
