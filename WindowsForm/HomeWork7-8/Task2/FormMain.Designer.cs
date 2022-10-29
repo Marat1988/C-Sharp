65c98466a3b6707447cc99d1d2943519a8e125c2
Marat <pirat03071988@mail.ru>
2022-10-29 12:27:06 +0200

    UPDATE

---
+++ WindowsForm/HomeWork7-8/Task2/FormMain.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Task2
+{
+    partial class FormPlay
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlayGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonPlayGame
+            // 
+            this.buttonPlayGame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonPlayGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlayGame.Location = new System.Drawing.Point(0, 0);
+            this.buttonPlayGame.Name = "buttonPlayGame";
+            this.buttonPlayGame.Size = new System.Drawing.Size(315, 191);
+            this.buttonPlayGame.TabIndex = 0;
+            this.buttonPlayGame.Text = "Загадай число от 1 до 2000 и нажми для запуска игры";
+            this.buttonPlayGame.UseVisualStyleBackColor = true;
+            this.buttonPlayGame.Click += new System.EventHandler(this.buttonPlayGame_Click);
+            // 
+            // FormPlay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 191);
+            this.Controls.Add(this.buttonPlayGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormPlay";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Угадай число";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlayGame;
+    }
+}
+
---
+++ WindowsForm/HomeWork7-8/Task2/FormMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class FormPlay : Form
+    {
+        private Random random = new Random();
+        private int number = 0;
+        public FormPlay()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPlayGame_Click(object sender, EventArgs e)
+        {
+            int numberAttempts = 0;
+            while (true)
+            {
+                number = random.Next(1, 2001);
+                numberAttempts++;
+                if (MessageBox.Show($"Вы загадали число {number}?", this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    if (MessageBox.Show($"Число попыток {numberAttempts}. Хотите сыграть еще?", this.Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                        break;
+                }
+            }
+        }
+    }
+}
