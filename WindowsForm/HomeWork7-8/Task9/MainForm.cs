8a894ffea53b6fc7b8bbc1545567f91fbd5bbf48
Marat <pirat03071988@mail.ru>
2022-11-08 23:33:43 +0200

    UPDATE

---
+++ WindowsForm/HomeWork7-8/Task9/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task9
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void buttonSaveProfileTextFile_Click(object sender, EventArgs e)
+        {
+            string gender = groupBoxGender.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked).Text;
+            Profile profile = new Profile(textBoxSurName.Text, 
+                                          textBoxName.Text, 
+                                          textBoxMiddleName.Text,
+                                          gender,
+                                          dateTimePickerBirthday.Value,
+                                          textBoxAdditionalInfo.Text);
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Текстовый файлы (*.txt)|*.txt",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                {
+                    writer.Write(profile.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ WindowsForm/HomeWork7-8/Task9/Profile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    public class Profile
+    {
+        private string surName;
+        private string name;
+        private string middleName;
+        private string gender;
+        private DateTime birthday;
+        private string additionalInfo;
+
+        public Profile(string surName, string name, string middleName, string gender, DateTime birthday, string additionalInfo)
+        {
+            this.surName = surName;
+            this.name = name;
+            this.middleName = middleName;
+            this.gender = gender;
+            this.birthday = birthday;
+            this.additionalInfo = additionalInfo;
+        }
+
+        public override string ToString()
+        {
+            return "Фамилия:\t" + surName + "\n" +
+                   "Имя:\t" + name + "\n" +
+                   "Отчество:\t" + middleName + "\n" +
+                   "Пол:\t" + gender + "\n" +
+                   "Дата рождения:\t" + birthday.ToShortDateString() + "\n" +
+                   "Доп. информация: " + additionalInfo;
+        }
+
+    }
+}
