0196cf1edc3b69161203b8dab00159079bcdf506
Marat <108996479+Marat1988@users.noreply.github.com>
2022-11-09 09:31:48 +0200

    UPDATE

--- WindowsForm/HomeWork7-8/Task9/MainForm.Designer.cs
+++ WindowsForm/HomeWork7-8/Task9/MainForm.Designer.cs
@@ -35,6 +35,7 @@ namespace Task9
             this.dateTimePickerBirthday = new System.Windows.Forms.DateTimePicker();
             this.labelBirthday = new System.Windows.Forms.Label();
             this.groupBoxGender = new System.Windows.Forms.GroupBox();
+            this.radioButtonIt = new System.Windows.Forms.RadioButton();
             this.radioButtonWoman = new System.Windows.Forms.RadioButton();
             this.radioButtonMens = new System.Windows.Forms.RadioButton();
             this.textBoxMiddleName = new System.Windows.Forms.TextBox();
@@ -44,13 +45,13 @@ namespace Task9
             this.textBoxSurName = new System.Windows.Forms.TextBox();
             this.labelSurName = new System.Windows.Forms.Label();
             this.buttonSaveProfileTextFile = new System.Windows.Forms.Button();
-            this.radioButtonIt = new System.Windows.Forms.RadioButton();
             this.groupBoxProfile.SuspendLayout();
             this.groupBoxGender.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBoxProfile
             // 
+            this.groupBoxProfile.Controls.Add(this.buttonSaveProfileTextFile);
             this.groupBoxProfile.Controls.Add(this.textBoxAdditionalInfo);
             this.groupBoxProfile.Controls.Add(this.labelAdditionalInfo);
             this.groupBoxProfile.Controls.Add(this.dateTimePickerBirthday);
@@ -62,45 +63,51 @@ namespace Task9
             this.groupBoxProfile.Controls.Add(this.labelName);
             this.groupBoxProfile.Controls.Add(this.textBoxSurName);
             this.groupBoxProfile.Controls.Add(this.labelSurName);
-            this.groupBoxProfile.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxProfile.Location = new System.Drawing.Point(16, 15);
+            this.groupBoxProfile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBoxProfile.Name = "groupBoxProfile";
-            this.groupBoxProfile.Size = new System.Drawing.Size(283, 417);
+            this.groupBoxProfile.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBoxProfile.Size = new System.Drawing.Size(673, 392);
             this.groupBoxProfile.TabIndex = 0;
             this.groupBoxProfile.TabStop = false;
             this.groupBoxProfile.Text = "Анкета:";
             // 
             // textBoxAdditionalInfo
             // 
-            this.textBoxAdditionalInfo.Location = new System.Drawing.Point(122, 251);
+            this.textBoxAdditionalInfo.Location = new System.Drawing.Point(165, 179);
+            this.textBoxAdditionalInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxAdditionalInfo.Multiline = true;
             this.textBoxAdditionalInfo.Name = "textBoxAdditionalInfo";
             this.textBoxAdditionalInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxAdditionalInfo.Size = new System.Drawing.Size(147, 151);
-            this.textBoxAdditionalInfo.TabIndex = 10;
+            this.textBoxAdditionalInfo.Size = new System.Drawing.Size(195, 185);
+            this.textBoxAdditionalInfo.TabIndex = 6;
             // 
             // labelAdditionalInfo
             // 
             this.labelAdditionalInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelAdditionalInfo.Location = new System.Drawing.Point(15, 300);
+            this.labelAdditionalInfo.Location = new System.Drawing.Point(17, 193);
+            this.labelAdditionalInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelAdditionalInfo.Name = "labelAdditionalInfo";
-            this.labelAdditionalInfo.Size = new System.Drawing.Size(105, 42);
+            this.labelAdditionalInfo.Size = new System.Drawing.Size(140, 52);
             this.labelAdditionalInfo.TabIndex = 9;
             this.labelAdditionalInfo.Text = "Дополнительная информация:";
             // 
             // dateTimePickerBirthday
             // 
-            this.dateTimePickerBirthday.Location = new System.Drawing.Point(122, 215);
+            this.dateTimePickerBirthday.Location = new System.Drawing.Point(418, 68);
+            this.dateTimePickerBirthday.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePickerBirthday.Name = "dateTimePickerBirthday";
-            this.dateTimePickerBirthday.Size = new System.Drawing.Size(147, 20);
-            this.dateTimePickerBirthday.TabIndex = 8;
+            this.dateTimePickerBirthday.Size = new System.Drawing.Size(195, 22);
+            this.dateTimePickerBirthday.TabIndex = 4;
             // 
             // labelBirthday
             // 
             this.labelBirthday.AutoSize = true;
             this.labelBirthday.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelBirthday.Location = new System.Drawing.Point(13, 215);
+            this.labelBirthday.Location = new System.Drawing.Point(281, 70);
+            this.labelBirthday.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelBirthday.Name = "labelBirthday";
-            this.labelBirthday.Size = new System.Drawing.Size(103, 13);
+            this.labelBirthday.Size = new System.Drawing.Size(129, 17);
             this.labelBirthday.TabIndex = 7;
             this.labelBirthday.Text = "Дата рождения:";
             // 
@@ -109,20 +116,35 @@ namespace Task9
             this.groupBoxGender.Controls.Add(this.radioButtonIt);
             this.groupBoxGender.Controls.Add(this.radioButtonWoman);
             this.groupBoxGender.Controls.Add(this.radioButtonMens);
-            this.groupBoxGender.Location = new System.Drawing.Point(10, 152);
+            this.groupBoxGender.Location = new System.Drawing.Point(12, 112);
+            this.groupBoxGender.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBoxGender.Name = "groupBoxGender";
-            this.groupBoxGender.Size = new System.Drawing.Size(257, 48);
-            this.groupBoxGender.TabIndex = 6;
+            this.groupBoxGender.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBoxGender.Size = new System.Drawing.Size(333, 59);
+            this.groupBoxGender.TabIndex = 5;
             this.groupBoxGender.TabStop = false;
             this.groupBoxGender.Text = "Пол:";
+            // 
+            // radioButtonIt
+            // 
+            this.radioButtonIt.AutoSize = true;
+            this.radioButtonIt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonIt.Location = new System.Drawing.Point(271, 23);
+            this.radioButtonIt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonIt.Name = "radioButtonIt";
+            this.radioButtonIt.Size = new System.Drawing.Size(59, 21);
+            this.radioButtonIt.TabIndex = 2;
+            this.radioButtonIt.Text = "Оно";
+            this.radioButtonIt.UseVisualStyleBackColor = true;
             // 
             // radioButtonWoman
             // 
             this.radioButtonWoman.AutoSize = true;
             this.radioButtonWoman.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButtonWoman.Location = new System.Drawing.Point(100, 19);
+            this.radioButtonWoman.Location = new System.Drawing.Point(133, 23);
+            this.radioButtonWoman.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButtonWoman.Name = "radioButtonWoman";
-            this.radioButtonWoman.Size = new System.Drawing.Size(79, 17);
+            this.radioButtonWoman.Size = new System.Drawing.Size(95, 21);
             this.radioButtonWoman.TabIndex = 1;
             this.radioButtonWoman.Text = "Женский";
             this.radioButtonWoman.UseVisualStyleBackColor = true;
@@ -132,9 +154,10 @@ namespace Task9
             this.radioButtonMens.AutoSize = true;
             this.radioButtonMens.Checked = true;
             this.radioButtonMens.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButtonMens.Location = new System.Drawing.Point(6, 19);
+            this.radioButtonMens.Location = new System.Drawing.Point(8, 23);
+            this.radioButtonMens.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButtonMens.Name = "radioButtonMens";
-            this.radioButtonMens.Size = new System.Drawing.Size(78, 17);
+            this.radioButtonMens.Size = new System.Drawing.Size(93, 21);
             this.radioButtonMens.TabIndex = 0;
             this.radioButtonMens.TabStop = true;
             this.radioButtonMens.Text = "Мужской";
@@ -142,84 +165,80 @@ namespace Task9
             // 
             // textBoxMiddleName
             // 
-            this.textBoxMiddleName.Location = new System.Drawing.Point(87, 113);
+            this.textBoxMiddleName.Location = new System.Drawing.Point(116, 70);
+            this.textBoxMiddleName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxMiddleName.Name = "textBoxMiddleName";
-            this.textBoxMiddleName.Size = new System.Drawing.Size(180, 20);
-            this.textBoxMiddleName.TabIndex = 5;
+            this.textBoxMiddleName.Size = new System.Drawing.Size(133, 22);
+            this.textBoxMiddleName.TabIndex = 3;
             // 
             // labelMiddleName
             // 
             this.labelMiddleName.AutoSize = true;
             this.labelMiddleName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelMiddleName.Location = new System.Drawing.Point(7, 116);
+            this.labelMiddleName.Location = new System.Drawing.Point(10, 70);
+            this.labelMiddleName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelMiddleName.Name = "labelMiddleName";
-            this.labelMiddleName.Size = new System.Drawing.Size(66, 13);
+            this.labelMiddleName.Size = new System.Drawing.Size(84, 17);
             this.labelMiddleName.TabIndex = 4;
             this.labelMiddleName.Text = "Отчество:";
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(87, 68);
+            this.textBoxName.Location = new System.Drawing.Point(418, 38);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(180, 20);
-            this.textBoxName.TabIndex = 3;
+            this.textBoxName.Size = new System.Drawing.Size(133, 22);
+            this.textBoxName.TabIndex = 2;
             // 
             // labelName
             // 
             this.labelName.AutoSize = true;
             this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelName.Location = new System.Drawing.Point(7, 71);
+            this.labelName.Location = new System.Drawing.Point(367, 38);
+            this.labelName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(36, 13);
+            this.labelName.Size = new System.Drawing.Size(43, 17);
             this.labelName.TabIndex = 2;
             this.labelName.Text = "Имя:";
             // 
             // textBoxSurName
             // 
-            this.textBoxSurName.Location = new System.Drawing.Point(87, 27);
+            this.textBoxSurName.Location = new System.Drawing.Point(116, 33);
+            this.textBoxSurName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxSurName.Name = "textBoxSurName";
-            this.textBoxSurName.Size = new System.Drawing.Size(180, 20);
+            this.textBoxSurName.Size = new System.Drawing.Size(133, 22);
             this.textBoxSurName.TabIndex = 1;
             // 
             // labelSurName
             // 
             this.labelSurName.AutoSize = true;
             this.labelSurName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelSurName.Location = new System.Drawing.Point(7, 30);
+            this.labelSurName.Location = new System.Drawing.Point(9, 37);
+            this.labelSurName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelSurName.Name = "labelSurName";
-            this.labelSurName.Size = new System.Drawing.Size(67, 13);
+            this.labelSurName.Size = new System.Drawing.Size(82, 17);
             this.labelSurName.TabIndex = 0;
             this.labelSurName.Text = "Фамилия:";
             // 
             // buttonSaveProfileTextFile
             // 
             this.buttonSaveProfileTextFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonSaveProfileTextFile.Location = new System.Drawing.Point(12, 447);
+            this.buttonSaveProfileTextFile.Location = new System.Drawing.Point(408, 306);
+            this.buttonSaveProfileTextFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSaveProfileTextFile.Name = "buttonSaveProfileTextFile";
-            this.buttonSaveProfileTextFile.Size = new System.Drawing.Size(282, 47);
-            this.buttonSaveProfileTextFile.TabIndex = 1;
+            this.buttonSaveProfileTextFile.Size = new System.Drawing.Size(240, 58);
+            this.buttonSaveProfileTextFile.TabIndex = 7;
             this.buttonSaveProfileTextFile.Text = "Сохранить в файл";
             this.buttonSaveProfileTextFile.UseVisualStyleBackColor = true;
             this.buttonSaveProfileTextFile.Click += new System.EventHandler(this.buttonSaveProfileTextFile_Click);
             // 
-            // radioButtonIt
-            // 
-            this.radioButtonIt.AutoSize = true;
-            this.radioButtonIt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.radioButtonIt.Location = new System.Drawing.Point(203, 19);
-            this.radioButtonIt.Name = "radioButtonIt";
-            this.radioButtonIt.Size = new System.Drawing.Size(48, 17);
-            this.radioButtonIt.TabIndex = 2;
-            this.radioButtonIt.Text = "Оно";
-            this.radioButtonIt.UseVisualStyleBackColor = true;
-            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(313, 509);
-            this.Controls.Add(this.buttonSaveProfileTextFile);
+            this.ClientSize = new System.Drawing.Size(702, 433);
             this.Controls.Add(this.groupBoxProfile);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Анкета";
