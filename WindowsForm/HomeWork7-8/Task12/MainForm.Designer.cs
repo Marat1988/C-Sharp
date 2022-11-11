8710316b3788e4fd61c35d2f0d795e0a13c62528
Marat <108996479+Marat1988@users.noreply.github.com>
2022-11-11 13:36:27 +0200

    UPDATE

--- WindowsForm/HomeWork7-8/Task12/MainForm.Designer.cs
+++ WindowsForm/HomeWork7-8/Task12/MainForm.Designer.cs
@@ -67,7 +67,7 @@ namespace Task12
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(998, 404);
+            this.ClientSize = new System.Drawing.Size(998, 438);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.textBoxDescription);
             this.Controls.Add(this.buttonLoadTextFile);
